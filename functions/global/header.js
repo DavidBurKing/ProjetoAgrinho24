9b21ecaffaad37795e16113cc8a159e81a35c2bf
Davi Ribeiro Berçocano <daviribeirobercocano@gmail.com>
2024-05-27 17:02:53 -0300

    teste1

---
+++ functions/global/header.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector('nav ul');
+let menuIcon = document.querySelector('.menu-icon');
+
+function abrirMenu(){
+    document.querySelector('nav ul').classList.toggle('open');
+}
