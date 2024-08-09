7c03b731747c72d74308a3c559288668953117cb
DavidBurKing <87403527+DavidBurKing@users.noreply.github.com>
2024-08-09 19:34:14 -0300

    Delete functions/global directory

--- functions/global/header.js
+++
@@ -1,6 +0,0 @@
-let menu = document.querySelector('nav ul');
-let menuIcon = document.querySelector('.menu-icon');
-
-function abrirMenu(){
-    document.querySelector('nav ul').classList.toggle('open');
-}
